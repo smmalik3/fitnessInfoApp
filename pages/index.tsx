e60134723a1f9b9044dfc82e82fd0ce0096ed1a7
Salman Malik <salmalik@deloitte.com>
2023-10-25 16:23:17 -0400

    got button toggles working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [activeSection, setActiveSection] = useState(null);
+
+  const toggleSection = (section) => {
+    if (activeSection === section) {
+      // If the same section is clicked again, hide it
+      setActiveSection(null);
+    } else {
+      // If a different section is clicked, show it
+      setActiveSection(section);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,11 +28,24 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.grid}>
           <div className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>Fitness Info App &rarr;</h2>
+            <p>Click the buttons dude</p>
+            <div className="btn-group" role="group" aria-label="Basic example">
+              <button onClick={() => toggleSection('GoodFood')} type="button" className="btn btn-secondary">Good Food</button>
+              <button onClick={() => toggleSection('BadFood')} type="button" className="btn btn-secondary">Bad Food</button>
+              <button onClick={() => toggleSection('TravelStrategy')} type="button" className="btn btn-secondary">Travel Strategy</button>
+              <button onClick={() => toggleSection('Occasions')} type="button" className="btn btn-secondary">Occasions</button>
+              <button onClick={() => toggleSection('Extra')} type="button" className="btn btn-secondary">Extra</button>
+            </div>
+
+            {activeSection === 'GoodFood' && <p>Good Food Section</p>}
+            {activeSection === 'BadFood' && <p>Bad Food Section</p>}
+            {activeSection === 'TravelStrategy' && <p>Travel Strategy Section</p>}
+            {activeSection === 'Occasions' && <p>Occasions Section</p>}
+            {activeSection === 'Extra' && <p>Extra Section</p>}
           </div>
         </div>
       </main>
     </div>
-  )
+  );
 }
